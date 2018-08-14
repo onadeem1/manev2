a0d2cb1374e54f2b8788ff8d717c9ced4d399fa9
Omer Nadeem <omernadeem3@gmail.com>
2018-08-14 16:42:02 -0400

    feat(database): update feed model to use scopes & only display required info

--- database/models/feed.js
+++ database/models/feed.js
@@ -1,39 +1,24 @@
 const sequelize = require('sequelize')
+
+//TODO: Set limits, offsets etc for loading feed? will see in future
+
 module.exports = db => {
   const Feed = db.define('feed', {})
 
-  Feed.writeFeed = async function(user, recommendationId) {
-    try {
-      const userIds = await user.getFriendAndUserIds()
-      const writeFeed = userIds.map(userId => {
-        Feed.upsert({ userId: userId, recommendationId: recommendationId })
-      })
-      return Promise.all(writeFeed)
-    } catch (error) {
-      console.error(error)
-    }
+  Feed.writeFeed = async function(user, postId) {
+    const userIds = await user.getFriendAndUserIds()
+    const writeFeed = userIds.map(userId => Feed.upsert({ userId, postId }))
+    return Promise.all(writeFeed)
   }
 
   Feed.loadFeed = async function(user) {
-    try {
-      const feed = await user.getFeedItems({
-        include: [
-          {
-            model: db.model('challenge'),
-            include: [{ model: db.model('user'), as: 'challengeCreator' }]
-          },
-          db.model('user'),
-          db.model('place')
-        ],
-        order: [[sequelize.col('feed.updatedAt'), 'DESC']]
-      })
-      const fullFeed = Promise.all(
-        feed.map(recommendation => recommendation.getRecommendationWithGoogPlace())
-      )
-      return fullFeed
-    } catch (error) {
-      console.error(error)
-    }
+    const feed = await user.getFeedPosts({
+      scope: ['place', 'user', 'linkedPost'],
+      include: [db.model('challenge').scope('challengeCreator')],
+      order: [[sequelize.col('feed.updatedAt'), 'DESC']]
+    })
+    //any way to limit this cause g is lame?
+    return Promise.all(feed.map(post => post.addGoog()))
   }
 
   return Feed
